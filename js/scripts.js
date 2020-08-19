dd80ab71cb2e7537619937f1a1758b958d413c9f
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-19 10:54:34 -0700

    TEST FOUR
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,15 @@ function isLeapYear(year) {
     return false;
   }
 }
+
+// TEST FOUR
+// Test: "It returns true for years that are divisible by 400"
+// Expect(isLeapYear(2000)).toEqual(true);
+
+function isLeapYear(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)|| (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
